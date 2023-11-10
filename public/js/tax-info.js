319187e181be635820b097ad5a05530d0b5f9f45
akashdeep023 <ad3500476@gmail.com>
2023-11-11 03:15:48 +0530

    update tax btn in mobile view

--- public/js/tax-info.js
+++ public/js/tax-info.js
@@ -25,3 +25,29 @@ for (let taxSwitch of taxSwitchs) {
     }
   });
 }
+let taxSwitchsS = document.querySelectorAll("#flexSwitchCheckDefaultS");
+for (let taxSwitch of taxSwitchsS) {
+  taxSwitch.addEventListener("click", () => {
+    let taxInfo = document.getElementsByClassName("tax-info");
+    let priceInfo = document.getElementsByClassName("price-info");
+    for (tax of taxInfo) {
+      if (tax.style.display != "inline") {
+        tax.style.display = "inline";
+        for (const changeA of changeAfter) {
+          changeA.innerHTML = "Display total after taxes ..";          
+        }
+      } else {
+        tax.style.display = "none";
+      }
+    }
+    for (price of priceInfo) {
+      if (price.style.display != "none") {
+        price.style.display = "none";
+      } else {
+        price.style.display = "inline";for (const changeA of changeAfter) {
+          changeA.innerHTML = "Display total before taxes ";          
+        }
+      }
+    }
+  });
+}
