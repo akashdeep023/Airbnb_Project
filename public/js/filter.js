f2fdf86b8c72f7a1d515e6e2acaf203cc3bfd9c3
akashdeep023 <ad3500476@gmail.com>
2023-11-13 19:21:58 +0530

    update filter.js file

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,15 +1,41 @@
 // filters icon ---------------
-let filter = document.querySelectorAll(".filter");
-for (const filt of filter) {
-  filt.addEventListener("click", function () {
-    for (const filt of filter) {
-      console.log("filter")
-      filt.style.opacity = "0.6";
-      filt.style.borderBottom = "none";
-      
-    }
-    filt.style.opacity = "1";
-    filt.style.borderBottom = "2.5px solid #000";
-  });
 
+const filters = document.querySelectorAll(".filter");
+
+// Function to apply styles based on the selected filter
+function applyStyles(selectedFilter) {
+  // Remove 'selected' class from all filters
+  filters.forEach((f) => f.classList.remove("selectedFilter"));
+  
+  // Add 'selected' class to the clicked filter
+  const selectedElement = document.querySelector("." + selectedFilter);
+
+
+  if (window.location.pathname === "/listings") {
+    filters[0].classList.add('selectedFilter')
+    return;
+  }
+  if (selectedElement) {
+    selectedElement.classList.add("selectedFilter");
+  }
+}
+
+// Add click event listener to each filter
+filters.forEach((filter) => {
+  filter.addEventListener("click", function () {
+    const element = this.classList[1];
+    console.log(element);
+
+    // Store the selected filter in localStorage
+    localStorage.setItem("selectedFilter", element);
+
+    // Apply styles
+    applyStyles(element);
+  });
+});
+
+// On page load, check if a filter was previously selected and apply styles
+const storedFilter = localStorage.getItem("selectedFilter");
+if (storedFilter) {
+  applyStyles(storedFilter);
 }
