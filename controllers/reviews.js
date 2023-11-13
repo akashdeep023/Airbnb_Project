96cfa006956395c688e359ce4bf673b8f7d027b5
akashdeep023 <ad3500476@gmail.com>
2023-11-13 19:14:44 +0530

    update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,10 +3,8 @@ const Review = require("../models/review.js");
 
 module.exports.createReview = async(req,res,next)=>{
     let listing = await Listing.findById(req.params.id);
-    console.log(listing)
     let newReview = new Review(req.body.review);
     newReview.author = req.user._id;
-    console.log(newReview);
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();      
