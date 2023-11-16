9297da81ef339c0324950f7c732cc18675700aec
akashdeep023 <ad3500476@gmail.com>
2023-11-16 20:58:16 +0530

    update js file

--- public/js/filterIcon.js
+++ public/js/filterIcon.js
@@ -1,8 +1,11 @@
 let page = document.querySelector(".pageblur");
 let containerbtn = document.querySelector(".showfilter");
-// let filtericon = document.querySelectorAll(".filterIcon");
 filterClick = () => {
-//   console.log("click");
   containerbtn.classList.add("filterbtnClick");
   page.classList.add("containerblur");
 };
+
+closefilter = () =>{
+  containerbtn.classList.remove("filterbtnClick");
+  page.classList.remove("containerblur");  
+}
