3543c5b13e5c5b5c4a4d1bab84442b5c090f6e68
akashdeep023 <ad3500476@gmail.com>
2023-12-22 16:15:00 +0530

    update filter.js

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,41 +1,46 @@
-// filters icon ---------------
-
+// filters icon -----------------------------------
 const filters = document.querySelectorAll(".filter");
-
 // Function to apply styles based on the selected filter
 function applyStyles(selectedFilter) {
-  // Remove 'selected' class from all filters
-  filters.forEach((f) => f.classList.remove("selectedFilter"));
-  
-  // Add 'selected' class to the clicked filter
-  const selectedElement = document.querySelector("." + selectedFilter);
-
-
-  if (window.location.pathname === "/listings") {
-    filters[0].classList.add('selectedFilter')
-    return;
-  }
-  if (selectedElement) {
-    selectedElement.classList.add("selectedFilter");
-  }
+	// Remove 'selected' class from all filters
+	filters.forEach((f) => f.classList.remove("selectedFilter"));
+	// Add 'selected' class to the clicked filter
+	const selectedElement = document.querySelector("." + selectedFilter);
+	if (window.location.pathname === "/listings") {
+		filters[0].classList.add("selectedFilter");
+		return;
+	}
+	if (selectedElement) {
+		selectedElement.classList.add("selectedFilter");
+	}
 }
 
 // Add click event listener to each filter
 filters.forEach((filter) => {
-  filter.addEventListener("click", function () {
-    const element = this.classList[1];
-    console.log(element);
-
-    // Store the selected filter in localStorage
-    localStorage.setItem("selectedFilter", element);
-
-    // Apply styles
-    applyStyles(element);
-  });
+	filter.addEventListener("click", function () {
+		const element = this.classList[1];
+		console.log(element);
+		// Store the selected filter in localStorage
+		localStorage.setItem("selectedFilter", element);
+		// Apply styles
+		applyStyles(element);
+	});
 });
-
 // On page load, check if a filter was previously selected and apply styles
 const storedFilter = localStorage.getItem("selectedFilter");
 if (storedFilter) {
-  applyStyles(storedFilter);
+	applyStyles(storedFilter);
 }
+
+
+// filters slide button add-----------------------------------
+let filtersBox = document.querySelector("#filters");
+let buttonSlide = document.querySelectorAll("#slideButton");
+
+buttonSlide.forEach((button) => {
+	button.addEventListener("click", () => {
+		const direction = button.className == "left_img_button" ? -1 : 1;
+		const scrollImg = direction * (filtersBox.clientWidth - 100);
+		filtersBox.scrollBy({ left: scrollImg, behavior: "smooth" });
+	});
+});
