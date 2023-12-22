0cb09d809f0c5b874cef4131973a2c9579d08b90
akashdeep023 <ad3500476@gmail.com>
2023-12-23 01:21:54 +0530

    update review.js

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const reviewSchema = new Schema({
     comment: {
         type: String,
-        unique: true,
+        // unique: true,
     },
     rating: {
         type: Number,
