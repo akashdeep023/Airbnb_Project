40fd8159b726e2da4a8f9adda92588c0aad46d2f
akashdeep023 <ad3500476@gmail.com>
2023-12-26 00:17:30 +0530

    update index.js

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
+	require("dotenv").config();
 }
 const mongoose = require("mongoose");
 const initData = require("./data.js");
@@ -8,25 +8,54 @@ const Listing = require("../models/listing.js");
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const DB_URL = process.env.ATLASDB_URL;
 main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => console.log(err));
+	.then(() => {
+		console.log("connected to DB");
+	})
+	.catch((err) => console.log(err));
 
 async function main() {
-  //   await mongoose.connect(MONGO_URL);
-  await mongoose.connect(DB_URL);
+	//   await mongoose.connect(MONGO_URL);
+	await mongoose.connect(DB_URL);
 }
 
+let categoryAll = [
+	"Beachfront",
+	"Cabins",
+	"Omg",
+	"Lake",
+	"Design",
+	"Amazing Pools",
+	"Farms",
+	"Amazing Views",
+	"Rooms",
+	"Lakefront",
+	"Tiny Homes",
+	"Countryside",
+	"Treehouse",
+	"Trending",
+	"Tropical",
+	"National Parks",
+	"Casties",
+	"Camping",
+	"Top Of The World",
+	"Luxe",
+	"Iconic Cities",
+	"Earth Homes",
+];
+
 const initDB = async () => {
-  await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({
-    ...obj,
-    owner: "654bcb1b695caafbad331f59",
-    price: obj.price * 25,
-    category:[`${Math.floor(Math.random()*22)+1}`,`${Math.floor(Math.random()*22)+1}`],
-  }));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+	await Listing.deleteMany({});
+	initData.data = initData.data.map((obj) => ({
+		...obj,
+		owner: "654bcb1b695caafbad331f59",
+		price: obj.price * 25,
+		category: [
+			`${categoryAll[Math.floor(Math.random() * 22)]}`,
+			`${categoryAll[Math.floor(Math.random() * 22)]}`,
+		],
+	}));
+	await Listing.insertMany(initData.data);
+	console.log("data was initialized");
 };
 initDB();
+
