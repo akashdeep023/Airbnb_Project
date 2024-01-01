5bfc92bcf5912fe9cfa0de23be044b22b2ccd0d0
akashdeep023 <ad3500476@gmail.com>
2024-01-02 02:07:11 +0530

    update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,70 +1,109 @@
 const User = require("../models/user");
 
-module.exports.renderSignupForm = (req,res)=>{
-    res.render("users/signup")
-}
+module.exports.renderSignupForm = (req, res) => {
+	res.render("users/signup");
+};
 
-module.exports.signUser = async(req,res,next)=>{
-    try{
-        let {username,email,password} = req.body;
-        const newUser = new User({email,username});
-        const registeredUser = await User.register(newUser,password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success","User was registered and Login");
-            res.redirect("/listings")
-        })
-    }catch(e){
-        req.flash("error",e.message)
-        res.redirect("/signup")
-    }
-}
+module.exports.signUser = async (req, res, next) => {
+	try {
+		let url = req.file.path;
+		let filename = req.file.filename;
+		let { fName, lName, username, email, password } = req.body;
+		const newUser = new User({ fName, lName, email, username });
+		newUser.image = { url, filename };
+		const registeredUser = await User.register(newUser, password);
+		console.log(registeredUser);
+		req.login(registeredUser, (err) => {
+			if (err) {
+				return next(err);
+			}
+			req.flash("success", "User was registered and Login");
+			res.redirect("/listings");
+		});
+	} catch (e) {
+		req.flash("error", e.message);
+		res.redirect("/signup");
+	}
+};
 
-module.exports.renderLoginForm = (req,res)=>{
-    res.render("users/login")
-}
+module.exports.renderLoginForm = (req, res) => {
+	res.render("users/login");
+};
 
-module.exports.loginUser = async(req,res,next)=>{
-    req.flash("success","Welcome back to Wanderlust! ")
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl)
-}
+module.exports.loginUser = async (req, res, next) => {
+	req.flash("success", "Welcome back to Wanderlust! ");
+	let redirectUrl = res.locals.redirectUrl || "/listings";
+	res.redirect(redirectUrl);
+};
 
-module.exports.logoutUser = (req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","You are logged out!");
-        res.redirect("/listings")
-    })
-}
+module.exports.logoutUser = (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		req.flash("success", "You are logged out!");
+		res.redirect("/listings");
+	});
+};
+
+module.exports.updateFormRender = async (req, res, next) => {
+	let { id } = req.params;
+	let user = await User.findById(id);
+	res.render("users/update.ejs", { user });
+};
+module.exports.updateAccount = async (req, res, next) => {
+	try {
+		let { id } = req.params;
+		let { fName, lName, username, email } = req.body;
+		const user = await User.findByIdAndUpdate(id, {
+			fName: fName,
+			lName: lName,
+			username: username,
+			email: email,
+		});
+
+		if (typeof req.file != "undefined") {
+			let url = req.file.path;
+			let filename = req.file.filename;
+			user.image = { url, filename };
+			await user.save();
+		}
+		req.flash("success", "Your account updated successfully.");
+		res.redirect("/profile");
+	} catch (e) {
+		req.flash("error", e.message);
+		res.redirect("/listings");
+	}
+};
+
+module.exports.updatePassword = async (req, res, next) => {
+	try {
+		let { id } = req.params;
+		const user = await User.findById(id);
+		await user.changePassword(req.body.oldPassword, req.body.newPassword);
+		await user.save();
+		req.flash(
+			"success",
+			"Your account password has been successfully updated."
+		);
+		res.redirect("/profile");
+	} catch (e) {
+		req.flash("error", e.message);
+		res.redirect("/listings");
+	}
+};
+
+module.exports.deleteAccount = async (req, res, next) => {
+	try {
+		let { id } = req.params;
+		await User.findByIdAndRemove(id);
+		req.logout();
+		req.flash("error", "Your account has been successfully deleted.");
+		res.redirect("/listings");
+	} catch (e) {
+		req.flash("error", e.message);
+		res.redirect("/listings");
+	}
+};
 
 
-
-
-
-
-
-
-
-
-module.exports.updateFormRender =async (req, res, next) => {
-    let { id } = req.params;
-    console.log(id)
-    let user = await User.findById(id);
-    res.render("users/update.ejs", {user})
-}
-module.exports.updateAccount =async (req, res, next) => {
-    let { id } = req.params;
-    console.log(id)
-    res.redirect("/listings")
-}
-module.exports.deleteAccount =async (req, res, next) => {
-    let { id } = req.params;
-    console.log(id)
-    res.redirect("/listings")
-}
