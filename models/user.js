dc3e4366ad638fe69bbde365546ad0e6b220c9a0
akashdeep023 <ad3500476@gmail.com>
2024-01-02 02:09:01 +0530

    update user.js

--- models/user.js
+++ models/user.js
@@ -2,14 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = Schema({ 
+const userSchema = Schema({
     email:{
         type: String,
         required: true
-    }
-   
+    },
+    fName:{
+        type: String,
+        required: true
+    },
+    lName:{
+        type: String,
+        required: true
+    },
+    image: {
+		url: String,
+		filename: String,
+	},
 })
 
-userSchema.plugin(passportLocalMongoose); 
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
