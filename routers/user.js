c21fe7018683dd5ed654adc7973ba95ad41ac94c
akashdeep023 <ad3500476@gmail.com>
2024-01-02 02:17:58 +0530

    update user.js

--- routers/user.js
+++ routers/user.js
@@ -2,14 +2,19 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware");
+const { saveRedirectUrl,isLoggedIn,isProfileOwner } = require("../middleware");
 
 const userController = require("../controllers/users")
+
+const multer  = require('multer')            //install multer package in npm || multipart/form-data type receive and paras
+// const upload = multer({ dest: 'uploads/' })  //uploads folder me save karega
+const {storage} = require("../cloudConfig.js")
+const upload = multer({storage});
 
 // Router.route-----------------------
 router.route("/signup")
 .get(userController.renderSignupForm)               //signup-----------
-.post(wrapAsync(userController.signUser))
+.post(upload.single('image'),wrapAsync(userController.signUser))
 
 
 
@@ -24,11 +29,13 @@ router.get("/logout",userController.logoutUser)     //logout----------
 router.get("/", (req, res) => {                     //redirect home page---------
     res.redirect("/listings")
 })
-router.get("/account-update/:id", wrapAsync(userController.updateFormRender))  //update  form render------------
+router.get("/update/:id",isLoggedIn,isProfileOwner, wrapAsync(userController.updateFormRender))  //update  form render------------
 
-router.post("/account-update/:id", wrapAsync(userController.updateAccount))  //update account------------
+router.post("/update/:id",isLoggedIn,isProfileOwner,upload.single('image'), wrapAsync(userController.updateAccount))  //update account------------
 
-router.post("/account-delete/:id", wrapAsync(userController.deleteAccount))  //delete account------------
+router.post("/change/:id", isLoggedIn, isProfileOwner, wrapAsync(userController.updatePassword))  //update password ------------
+
+router.delete("/delete/:id",isLoggedIn,isProfileOwner, wrapAsync(userController.deleteAccount))  //delete account------------
 
 module.exports = router;                            //export---app.js---------------
 
