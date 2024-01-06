857d37b192940def20145c5880a75617efd8d0da
akashdeep023 <ad3500476@gmail.com>
2024-01-06 21:42:26 +0530

    update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,26 @@
 (() => {
-    'use strict'
-    const forms = document.querySelectorAll('.needs-validation')
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+	"use strict";
+	const forms = document.querySelectorAll(".needs-validation");
+	Array.from(forms).forEach((form) => {
+		form.addEventListener(
+			"submit",
+			(event) => {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+					console.log("script.js = ", event);
+				}
+				else {
+					document.querySelector("#loder").style.display = "inline"
+					document.querySelector(".pageblur").style.opacity = ".3"
+					event.submitter.disabled = true;
+					event.submitter.innerHTML = "Loading.."
+					event.submitter.style.fontSize = ".8rem"
+				}
+				form.classList.add("was-validated");
+			},
+			false,
+		);
+	});
+})();
+
