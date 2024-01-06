32a8e073105a496b3d088998cfa050a195cb52c2
akashdeep023 <ad3500476@gmail.com>
2024-01-06 22:07:09 +0530

    update schema.js

--- schema.js
+++ schema.js
@@ -1,27 +1,28 @@
-const Joi = require ("joi");
+const Joi = require("joi");
 
 module.exports.listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        country: Joi.string().required(),
-        location: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("",null),
-        category: Joi.allow(""),
-    }).required()
-})
+	listing: Joi.object({
+		title: Joi.string().required(),
+		description: Joi.string().required(),
+		country: Joi.string().required(),
+		location: Joi.string().required(),
+		price: Joi.number().required().min(0),
+		image: Joi.string().allow("", null),
+		category: Joi.allow(""),
+	}).required(),
+});
 
 module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        comment: Joi.string().required(),
-    }).required()
-})
+	review: Joi.object({
+		rating: Joi.number().required().min(1).max(5),
+		comment: Joi.string().required(),
+	}).required(),
+});
 
-
-
-
-
-
-
+module.exports.userSchema = Joi.object({
+    fName: Joi.string().required().trim(),
+    lName: Joi.string().required().trim(),
+    username: Joi.string().required(),
+	email: Joi.string().required().email(),
+    password: Joi.string().min(8),
+});
