dd1ce2e72afc5e2b7eb2a7066be9f43d4c22b042
akashdeep023 <ad3500476@gmail.com>
2024-01-13 23:48:10 +0530

    update update.js

--- public/js/update.js
+++ public/js/update.js
@@ -6,21 +6,18 @@ let passwordFind = document.querySelector("#password-find")
 let detailFind = document.querySelector("#detail-find")
 let imageFind = document.querySelector("#image-find")
 
-passwordFind.addEventListener("click",()=> {
-    console.log("click")
-    passwordBox.style.display = "inline"
-    detailBox.style.display = "none"
-    imageBox.style.display = "none"
-})
 detailFind.addEventListener("click",()=> {
-    console.log("click")
     passwordBox.style.display = "none"
     detailBox.style.display = "inline"
     imageBox.style.display = "none"
 })
 imageFind.addEventListener("click",()=> {
-    console.log("click")
+    imageBox.style.display = "inline"
     passwordBox.style.display = "none"
     detailBox.style.display = "none"
-    imageBox.style.display = "inline"
+})
+passwordFind.addEventListener("click",()=> {
+    passwordBox.style.display = "inline"
+    detailBox.style.display = "none"
+    imageBox.style.display = "none"
 })
