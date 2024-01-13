2814148aeff165a5f9bec137dbf87212f8cc38a4
akashdeep023 <ad3500476@gmail.com>
2024-01-14 00:06:45 +0530

    add google.js

---
+++ routers/google.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const passport = require('../controllers/googles.js')
+
+router.get(
+	"/",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+	"/callback",
+	passport.authenticate("google", { failureRedirect: "/login" }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		res.redirect("/");
+	}
+);
+
+module.exports = router;
